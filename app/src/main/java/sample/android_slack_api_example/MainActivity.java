18c666731c087db870f6455102b8d93b8340506a
WataruSuzuki <wataru0406@gmail.com>
2017-09-01 09:51:41 +0900

    Added sample code : get emoji list

--- app/src/main/java/sample/android_slack_api_example/MainActivity.java
+++ app/src/main/java/sample/android_slack_api_example/MainActivity.java
@@ -1,13 +1,65 @@
 package sample.android_slack_api_example;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Map;
+
+import allbegray.slack.SlackClientFactory;
+import allbegray.slack.webapi.SlackWebApiClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String slackToken = "";
+    private SlackWebApiClient mWebApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] items = {"Auth", "Get emoji list", "Set status"};
+        ListView lv = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == MySampleMenu.EMOJI.ordinal()) {
+                    getEmojiList();
+                }
+            }
+        });
+
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
+    }
+
+    private void getEmojiList() {
+        mWebApiClient = SlackClientFactory.createWebApiClient(slackToken);
+        Map<String, String> emojiList =  mWebApiClient.getEmojiList();
+        System.out.println(emojiList.toString());
+    }
+
+    private enum MySampleMenu {
+        AUTH("Auth"),
+        EMOJI("Get emoji list"),
+        STATUS("Set status");
+
+        private final String name;
+
+        private MySampleMenu(final String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 }
